c733c54c25a19d2695b74c57ad63fcf51f53c42b
Ryan <rcouture27@gmail.com>
2020-04-20 18:50:12 -0400

    inital ph code

--- main/main.ino
+++ main/main.ino
@@ -2,14 +2,19 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
 #include "Temperature.h"
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
 
-// ph vars
-//const int analogInPin = 36;
-//int sensorValue = 0;
-//unsigned long int avgValue;
-//float b;
-//int buf[10], temp;
+#define rx 0                                          //define what pin rx is going to be
+#define tx 0                                          //define what pin tx is going to be
 
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+float pH;                                             //used to hold a floating point number that is the pH
 // initialize libraries
 #define ONE_WIRE_BUS 26
 OneWire oneWire(ONE_WIRE_BUS);
@@ -19,17 +24,55 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 Temperature temp(&lcd, &sensors);
 
 void setup(void) {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
   sensors.begin();
 
   // configure lcd
   lcd.begin(16, 2);
   lcd.display();
+
+  // configure ph
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
 }
 
 void loop(void) {
   // Display and update temperature as needed
   temp.temperatureDisplay();
+
+   if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
+    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+    /*                                                //uncomment this section to see how to convert the pH reading from a string to a float 
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      pH = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      if (pH >= 7.0) {                                //if the pH is greater than or equal to 7.0
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
+      }
+      if (pH <= 6.999) {                              //if the pH is less than or equal to 6.999
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
+      }
+    }
+    */
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
 }
 
 //void ATORelay() {
@@ -42,16 +85,4 @@ void loop(void) {
 //    } else {
 //      digitalWrite(outlet, LOW);
 //    }
-//}
-
-//void phAndRoomTempReading() {
-//  // read the input on analog pin 0:
-//  int sensorValue = analogRead(36);
-//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-////  float voltage = sensorValue * (5.0 / 1023.0);
-////  float voltage = (float) sensorValue / (4096 * 3.3);
-//  // print out the value you read:
-//  float voltage = (float) sensorValue * (3.3 / 4095);
-//  Serial.println(sensorValue);
-//  delay(300);
 //}
