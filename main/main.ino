c9f7e3729a4b1399b5cadec62fe00ecd834e611d
Ryan <rcouture27@gmail.com>
2022-08-06 23:17:10 -0400

    testing http request

--- main/main.ino
+++ main/main.ino
@@ -1,47 +1,117 @@
+//#include <ArduinoWiFiServer.h>
+//#include <BearSSLHelpers.h>
+//#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+//#include <ESP8266WiFiAP.h>
+//#include <ESP8266WiFiGeneric.h>
+//#include <ESP8266WiFiGratuitous.h>
+//#include <ESP8266WiFiMulti.h>
+//#include <ESP8266WiFiScan.h>
+//#include <ESP8266WiFiSTA.h>
+//#include <ESP8266WiFiType.h>
+
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+//#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+//#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+
+
+
+//#include <LiquidCrystal.h>
+//#include "SSD1306Wire.h" 
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+//#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include "Temperature.h"
-#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
-
-#define rx 0                                          //define what pin rx is going to be
-#define tx 0                                          //define what pin tx is going to be
-
-SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
 
 
-String inputstring = "";                              //a string to hold incoming data from the PC
-String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
-boolean input_string_complete = false;                //have we received all the data from the PC
-boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
-float pH;                                             //used to hold a floating point number that is the pH
-// initialize libraries
+#define rx 9                                         //define what pin rx is going to be
+#define tx 8                                         //define what pin tx is going to be
+
+int count = 0;
+
+const int buttonPin = 12;
+int buttonState = 0;                                  // variable for reading the pushbutton status
+
+/** pH variables */
+String inputstring = "";                              // a string to hold incoming data from the PC
+String sensorstring = "";                             // a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                // have we received all the data from the PC
+boolean sensor_string_complete = false;               // have we received all the data from the Atlas Scientific product
+float pH; 
+
+/** http vars */
+int    HTTP_PORT   = 3000;
+String HTTP_METHOD = "GET";
+char   HOST_NAME[] = "71.232.236.213"; // hostname of web server:
+String PATH_NAME   = "/phs";
+
+//SSD1306Wire display(0x3c, SDA, SCL);                  // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h
+
+/** initialize libraries */
 #define ONE_WIRE_BUS 26
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-const int rs = 19, en = 23, d4 = 27, d5 = 16, d6 = 17, d7 = 25;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-Temperature temp(&lcd, &sensors);
+SoftwareSerial myserial(rx, tx);
+HTTPClient http;
+WiFiClient client;
+
+//const int rs = 19, en = 23, d4 = 27, d5 = 16, d6 = 17, d7 = 25;
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+//Temperature temp(&lcd, &sensors);
 
 void setup(void) {
+  myserial.begin(9600);
   Serial.begin(9600);
-  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
   sensors.begin();
 
-  // configure lcd
-  lcd.begin(16, 2);
-  lcd.display();
+  // configure button
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  // configure ph
+  // configure lcd
+//  lcd.begin(16, 2);
+//  lcd.display();
+
+  /** configure ph */
   inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
   sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+
+  testRequest();
 }
 
 void loop(void) {
-  // Display and update temperature as needed
-  temp.temperatureDisplay();
+//  temp.temperatureDisplay();                          //  Display and update temperature as needed
 
-   if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
+  readPh();
+  
+/** configure wifiscan */
+//  WiFi.mode(WIFI_STA);
+//  WiFi.disconnect();
+
+//  wifiScan();
+  
+//  delay(100);
+
+/** configure display */
+//  display.init();
+//  display.flipScreenVertically();
+//  display.setFont(ArialMT_Plain_10);
+}
+
+void serialEvent() {                                  // if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           // read the string until we see a <CR>
+  input_string_complete = true;                       // set the flag used to tell if we have received a completed string from the PC
+}
+
+void readPh() {
+  if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
     myserial.print(inputstring);                      //send that string to the Atlas Scientific product
     myserial.print('\r');                             //add a <CR> to the end of the string
     inputstring = "";                                 //clear the string
@@ -56,9 +126,8 @@ void loop(void) {
     }
   }
 
-
   if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+    Serial.println(sensorstring + " testing");        //send that string to the PC's serial monitor
     /*                                                //uncomment this section to see how to convert the pH reading from a string to a float 
     if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
       pH = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
@@ -74,6 +143,90 @@ void loop(void) {
     sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
   }
 }
+
+void testRequest() {
+  http.begin(client, "http://jsonplaceholder.typicode.com/users/1");
+  int httpCode = http.GET();
+  String payload = http.getString();
+  Serial.println(payload);
+  http.end();
+}
+
+
+
+
+/************************************************** OTHER **************************************************/
+
+
+//void loop(void) {
+//  buttonState = digitalRead(buttonPin);
+//
+//  readPh();
+//  //temp.temperatureDisplay();                          // Display and update temperature as needed
+//
+//  //display.clear();
+//  
+//  if (buttonState == LOW) {
+//      // pressed                                 
+////      wifiScan();       
+//  } else {
+//    // Not pressed
+//  }
+//}
+
+
+//void wifiScan() {
+//   Serial.println("scan start");
+//
+//    int n = WiFi.scanNetworks();                      // number of networks found
+//    Serial.println("scan done");
+//    if (n == 0) {
+//        Serial.println("no networks found");
+//  } else {
+//        Serial.print(n);
+//        Serial.println(" networks found");
+//        int x = 0, y = 15;
+//        for (int i = 0; i < n; ++i) {                 // Print SSID and RSSI for each network found   
+//            if (i == 0) {
+//              display.drawString(x, 0, WiFi.SSID(i));
+//            } else if (i == 1) {
+//              display.drawString(x, 15, WiFi.SSID(i));
+//            } else if (i == 2) {
+//              display.drawString(x, 30, WiFi.SSID(i));
+//            }
+//            display.display();   
+//         
+//            Serial.print(i + 1);
+//            Serial.print(": ");
+//
+//            Serial.print(WiFi.SSID(i));
+//            Serial.print(" (");
+//            Serial.print(WiFi.RSSI(i));
+//            Serial.print(")");
+//            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?"Open":"Secure");
+//            delay(10);
+//        }
+//    }
+//    Serial.println("");
+//
+//    // Wait a bit before scanning again
+//    delay(5000);
+//} 
+
+
+
+
+//void drawFontFaceDemo() {
+//    // Font Demo1
+//    // create more fonts at http://oleddisplay.squix.ch/
+//    display.setTextAlignment(TEXT_ALIGN_LEFT);
+//    display.setFont(ArialMT_Plain_10);
+//    display.drawString(0, 0, "Hello world");
+//    display.setFont(ArialMT_Plain_16);
+//    display.drawString(10, 10, "Hello world");
+//    display.setFont(ArialMT_Plain_24);
+//    display.drawString(0, 40, "Hello world");
+//}
 
 //void ATORelay() {
 //    put in setup 'pinMode'
